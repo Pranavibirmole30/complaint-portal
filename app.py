ff4f32d9a6cf68301a36c98c94e2ac4e2b525b42
Pranavibirmole30 <pranavibirmole@gmail.com>
2026-03-14 02:20:20 +0530

    db fix

--- app.py
+++ app.py
@@ -14,30 +14,42 @@ UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Database connection
+import os
+import sqlite3
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def get_db():
-    return sqlite3.connect("database.db")
+    db_path = os.path.join(BASE_DIR,"database.db")
+    return sqlite3.connect(db_path, check_same_thread=False)
 
 # Create tables
-with get_db() as db:
-    db.execute("""
-    CREATE TABLE IF NOT EXISTS users(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        email TEXT,
-        password TEXT
-    )
-    """)
-    db.execute("""
-    CREATE TABLE IF NOT EXISTS complaints(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        category TEXT,
-        description TEXT,
-        location TEXT,
-        status TEXT,
-        image TEXT
-    )
-    """)
+def init_db():
+    with get_db() as db:
 
+        db.execute("""
+        CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT,
+            password TEXT
+        )
+        """)
+
+        db.execute("""
+        CREATE TABLE IF NOT EXISTS complaints(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            category TEXT,
+            description TEXT,
+            location TEXT,
+            status TEXT,
+            image TEXT
+        )
+        """)
+
+        db.commit()
+
+init_db()
 @app.route('/')
 def index():
     return render_template("index.html")
